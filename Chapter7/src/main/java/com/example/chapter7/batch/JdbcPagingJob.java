a16dfe6b7b8c463cc8af3822269893b36d1c391c
KwangtaekJung <kt79.jung@gmail.com>
2023-01-09 23:54:16 +0900

    Chapter7-ItemReader(JDBC Paging)

--- Chapter7/src/main/java/com/example/chapter7/batch/JdbcPagingJob.java
+++ Chapter7/src/main/java/com/example/chapter7/batch/JdbcPagingJob.java
@@ -1,7 +1,6 @@
 package com.example.chapter7.batch;
 
 import com.example.chapter7.domain.CustomerDomain;
-import com.example.chapter7.domain.CustomerForXml;
 import com.example.chapter7.mapper.CustomerRowMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -11,45 +10,61 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 
 import javax.sql.DataSource;
-
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
-//@Configuration
-public class JdbcCursorJob {
+@Configuration
+public class JdbcPagingJob {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public JdbcCursorItemReader<CustomerDomain> customerItemReader(DataSource dataSource) {
-        return new JdbcCursorItemReaderBuilder<CustomerDomain>()
+    @StepScope
+    public JdbcPagingItemReader<CustomerDomain> customerItemReader(
+            DataSource dataSource,
+            PagingQueryProvider queryProvider,
+            @Value("#{jobParameters['city']}") String city) {
+
+        city = "Chicago"; //Test
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("city", city);
+
+        return new JdbcPagingItemReaderBuilder<CustomerDomain>()
                 .name("customerItemReader")
                 .dataSource(dataSource)
-//                .sql("select * from customer")
-                .sql("select * from customer where city = ?")
+                .queryProvider(queryProvider)
+                .parameterValues(parameterValues)
+                .pageSize(10)
                 .rowMapper(new CustomerRowMapper())
-                .preparedStatementSetter(citySetter(null))
                 .build();
     }
 
     @Bean
-    @StepScope
-    public ArgumentPreparedStatementSetter citySetter(
-            @Value("#{jobParameters['city']}") String city) {
+    public SqlPagingQueryProviderFactoryBean pagingQueryProviderFactoryBean(DataSource dataSource) {
+        SqlPagingQueryProviderFactoryBean factoryBean = new SqlPagingQueryProviderFactoryBean();
 
-        return new ArgumentPreparedStatementSetter(new Object[] {city});
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setSelectClause("select *");
+        factoryBean.setFromClause("from Customer");
+        factoryBean.setWhereClause("where city = :city");
+        factoryBean.setSortKey("lastName");
+
+        return factoryBean;
     }
 
     @Bean
-    public ItemWriter itemWriter() {
+    public ItemWriter<CustomerDomain> itemWriter() {
         return (items) -> items.forEach(System.out::println);
     }
 
@@ -57,14 +72,14 @@ public class JdbcCursorJob {
     public Step copyFileStep() {
         return this.stepBuilderFactory.get("copyFileStep")
                 .<CustomerDomain, CustomerDomain>chunk(10)
-                .reader(customerItemReader(null))
+                .reader(customerItemReader(null, null, null))
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("job")
+        return this.jobBuilderFactory.get("JdbcPagingJob")
                 .start(copyFileStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
