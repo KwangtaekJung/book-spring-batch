b7b3c677e8b9e9296246dd2669d0d389ab8d68cd
KwangtaekJung <kt79.jung@gmail.com>
2023-01-10 00:56:32 +0900

    Chapter7-ItemReader(JPA)

--- Chapter7/src/main/java/com/example/chapter7/batch/JpaJob.java
+++ Chapter7/src/main/java/com/example/chapter7/batch/JpaJob.java
@@ -1,6 +1,7 @@
 package com.example.chapter7.batch;
 
 import com.example.chapter7.domain.CustomerEntity;
+import com.example.chapter7.util.CustomerByCityQueryProvider;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.SessionFactory;
 import org.springframework.batch.core.Job;
@@ -10,10 +11,10 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.HibernateCursorItemReader;
 import org.springframework.batch.item.database.HibernatePagingItemReader;
-import org.springframework.batch.item.database.builder.HibernateCursorItemReaderBuilder;
+import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.HibernatePagingItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,25 +23,43 @@ import javax.persistence.EntityManagerFactory;
 import java.util.Collections;
 
 @RequiredArgsConstructor
-//@Configuration
-public class HibernatePagingJob {
+@Configuration
+public class JpaJob {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+//    @Bean
+//    @StepScope
+//    public JpaPagingItemReader<CustomerEntity> customerItemReader(
+//            EntityManagerFactory entityManagerFactory,
+//            @Value("#{jobParameters['city']}") String city) {
+//
+//        city = "Chicago";
+//        return new JpaPagingItemReaderBuilder<CustomerEntity>()
+//                .name("customerItemReader")
+//                .entityManagerFactory(entityManagerFactory)
+//                .queryString("select c from CustomerEntity c where c.city = :city")
+//                .parameterValues(Collections.singletonMap("city", city))
+//                .build();
+//    }
+
     @Bean
     @StepScope
-    public HibernatePagingItemReader<CustomerEntity> customerItemReader(
+    public JpaPagingItemReader<CustomerEntity> customerItemReader(
             EntityManagerFactory entityManagerFactory,
             @Value("#{jobParameters['city']}") String city) {
 
         city = "Chicago";
-        return new HibernatePagingItemReaderBuilder<CustomerEntity>()
+
+        CustomerByCityQueryProvider queryProvider = new CustomerByCityQueryProvider();
+        queryProvider.setCityName(city);
+
+        return new JpaPagingItemReaderBuilder<CustomerEntity>()
                 .name("customerItemReader")
-                .sessionFactory(entityManagerFactory.unwrap(SessionFactory.class))
-                .queryString("from CustomerEntity where city = :city")
+                .entityManagerFactory(entityManagerFactory)
+                .queryProvider(queryProvider)
                 .parameterValues(Collections.singletonMap("city", city))
-                .pageSize(10)
                 .build();
     }
 
@@ -60,7 +79,7 @@ public class HibernatePagingJob {
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("HibernatePagingJob")
+        return this.jobBuilderFactory.get("JpaJob")
                 .start(copyFileStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
