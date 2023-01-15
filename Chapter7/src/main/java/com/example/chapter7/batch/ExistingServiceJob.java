5220b8ff4dc369cb65a1cba48fb2921418f2379c
KwangtaekJung <kt79.jung@gmail.com>
2023-01-15 20:05:27 +0900

    Chapter7-ItemReader(기존 서비스)

--- Chapter7/src/main/java/com/example/chapter7/batch/ExistingServiceJob.java
+++ Chapter7/src/main/java/com/example/chapter7/batch/ExistingServiceJob.java
@@ -1,46 +1,33 @@
 package com.example.chapter7.batch;
 
 import com.example.chapter7.domain.CustomerEntity;
-import com.example.chapter7.util.CustomerRepository;
+import com.example.chapter7.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.item.adapter.ItemReaderAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
-
-import java.util.Collections;
 
 @RequiredArgsConstructor
-//@Configuration
-public class SpringDataRepositoryJob {
+@Configuration
+public class ExistingServiceJob {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    @StepScope
-    public RepositoryItemReader<CustomerEntity> customerItemReader(
-            CustomerRepository repository,
-            @Value("jobParameters['city']}") String city) {
+    public ItemReaderAdapter<CustomerEntity> customerItemReader(CustomerService customerService) {
+        ItemReaderAdapter<CustomerEntity> adapter = new ItemReaderAdapter<>();
 
-        city = "Chicago";
-        return new RepositoryItemReaderBuilder<CustomerEntity>()
-                .name("customerItemReader")
-                .arguments(Collections.singleton(city))
-                .methodName("findByCity")
-                .repository(repository)
-                .sorts(Collections.singletonMap("lastName", Sort.Direction.ASC))
-                .pageSize(10)
-                .build();
+        adapter.setTargetObject(customerService);
+        adapter.setTargetMethod("getCustomer");
+
+        return adapter;
     }
 
     @Bean
@@ -52,14 +39,14 @@ public class SpringDataRepositoryJob {
     public Step copyFileStep() {
         return this.stepBuilderFactory.get("copyFileStep")
                 .<CustomerEntity, CustomerEntity>chunk(10)
-                .reader(customerItemReader(null, null))
+                .reader(customerItemReader(null ))
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("RepositoryJob")
+        return this.jobBuilderFactory.get("ExistingServiceJob")
                 .start(copyFileStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
